0fe1c0aee4830dd90b78cbbdac423e0c7fc0f416
Peter Huene <peter@huene.dev>
2023-03-27 18:35:32 -0700

    Update.

--- proxy/src/lib.rs
+++ proxy/src/lib.rs
@@ -1,4 +1,5 @@
 use bindings::{
+    app_app::{render, AppPropsParam},
     http::{self, IncomingRequest, ResponseOutparam},
     incoming_http::IncomingHttp,
     streams,
@@ -7,11 +8,16 @@ use bindings::{
 struct Component;
 
 impl IncomingHttp for Component {
-    fn handle(request: IncomingRequest, _response_out: ResponseOutparam) {
-        let headers = http::new_fields(&[("Content-Type", "text/plain")]);
+    fn handle(_: IncomingRequest, _: ResponseOutparam) {
+        let html = render(AppPropsParam {
+            name: "Cloud Native Wasm Day!",
+            count: 5,
+        });
+        
+        let headers = http::new_fields(&[("Content-Type", "text/html")]);
         let response = http::new_outgoing_response(200, headers);
         let stream = http::outgoing_response_write(response).unwrap();
-        streams::write(stream, "Hello world!".as_bytes()).unwrap();
+        streams::write(stream, html.as_bytes()).unwrap();
         http::set_response_outparam(Ok(response)).unwrap();
     }
 }
